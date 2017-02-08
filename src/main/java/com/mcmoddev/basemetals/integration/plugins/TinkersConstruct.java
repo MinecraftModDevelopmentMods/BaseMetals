4f69b4e58b1755a8cde24d944f38936377988135
Daniel Hazelton <dshadowwolf@gmail.com>
2017-02-08 08:47:08 -0500

    Remove commented out code that was not coming back because of TiC and switch all remaining materials to use the TCMetalMaterial interface.

--- src/main/java/com/mcmoddev/basemetals/integration/plugins/TinkersConstruct.java
+++ src/main/java/com/mcmoddev/basemetals/integration/plugins/TinkersConstruct.java
@@ -26,6 +26,11 @@ public class TinkersConstruct extends com.mcmoddev.lib.integration.plugins.Tinke
 		}
 
 		if (Options.enableAdamantine) {
+			/*
+			 * Newly Suggested:
+			 * Savage (ignore up to 10 points of damage)
+			 * Petrarmor
+			 */
 			TCMetalMaterial adamantTC = new TCMetalMaterial(Materials.adamantine);
 			adamantTC.craftable = false;
 			adamantTC.addTrait(new TraitColdblooded());
@@ -34,118 +39,147 @@ public class TinkersConstruct extends com.mcmoddev.lib.integration.plugins.Tinke
 		}
 
 		if (Options.enableAntimony) {
-			registerMaterial(Materials.antimony.getName(), false, true);
+			TCMetalMaterial antimonyTC = new TCMetalMaterial(Materials.antimony);
+			antimonyTC.craftable = false;
+			
+			registerMaterial(antimonyTC);
 		}
 
 		if (Options.enableAquarium) {
-			registerMaterial(Materials.aquarium, false, true, new TraitAquadynamic());
+			TCMetalMaterial aquaTC = new TCMetalMaterial(Materials.aquarium);
+			aquaTC.craftable = false;
+			aquaTC.addTrait(new TraitAquadynamic());
+			
+			registerMaterial(aquaTC);
+			// When FMe is out we can probably do the alloy.
 			// registerAlloy(Materials.aquarium.getName(), 3,
 			//		new String[] { "copper", "zinc", "prismarine" },
 			//		new int[] { 2, 1, 3}); // Not possible currently
 		}
 
 		if (Options.enableBismuth) {
-			registerMaterial(Materials.bismuth.getName(), false, true);
+			TCMetalMaterial bismuthTC = new TCMetalMaterial(Materials.bismuth);
+			bismuthTC.craftable = false;
+			
+			registerMaterial(bismuthTC);
 		}
 
 		if (Options.enableBrass) {
-			registerMaterial(Materials.brass.getName(), false, true);
-			// registerAlloy(Materials.brass.getName(), 3,
-			//		new String[] { "copper", "zinc" },
-			//		new int[] { 2, 1 }); // TCon already has Brass alloy
+			TCMetalMaterial brassTC = new TCMetalMaterial(Materials.brass);
+			brassTC.craftable = false;
+			
+			brassTC.addTrait(new TraitDense());
+			
+			registerMaterial(brassTC);
 		}
-
-		/*
-		if (Options.ENABLE_BRONZE) {
-			registerMaterial(Materials.bronze.getName(), false, true);
-			registerAlloy(Materials.bronze.getName(), 4,
-					new String[] { "copper", "tin" },
-					new int[] { 3, 1 }); // TCon already has Bronze alloy
-		}
-		*/
 
 		if (Options.enableCoal) {
 			registerFluid(Materials.vanilla_coal, 144);
 		}
 		
 		if (Options.enableColdIron) {
-			registerMaterial(Materials.coldiron, false, true, new TraitFreezing());
+			TCMetalMaterial coldironTC = new TCMetalMaterial(Materials.coldiron);
+			coldironTC.craftable = false;
+			
+			coldironTC.addTrait(new TraitFreezing());
+			registerMaterial(coldironTC);
 		}
-
-		/*
-		if (Options.ENABLE_COPPER) {
-			registerMaterial(Materials.copper.getName(), false, true);
-		}
-		*/
 
 		if (Options.enableCupronickel) {
-			registerMaterial(Materials.cupronickel.getName(), false, true);
+			TCMetalMaterial cuproTC = new TCMetalMaterial(Materials.cupronickel);
+			cuproTC.craftable = false;
+			
+			registerMaterial(cuproTC);
 			registerAlloy(Materials.cupronickel.getName(), 4,
 					new String[] { "copper", "nickel" },
 					new int[] { 3, 1 });
 		}
 
-		/*
-		if (Options.ENABLE_ELECTRUM) {
-			registerMaterial(Materials.electrum.getName(), false, true);
-			registerAlloy(Materials.electrum.getName(), 2,
-					new String[] { "silver", "gold" },
-					new int[] { 2, 1 }); // TCon already has Electrum alloy
-		}
-		*/
-
 		if (Options.enableInvar) {
-			registerMaterial(Materials.invar.getName(), false, true);
-			// registerAlloy(Materials.invar.getName(), 3,
-			//		new String[] { "iron", "nickel" },
-			//		new int[] { 2, 1 }); // TCon already has Invar alloy
+			TCMetalMaterial invarTC = new TCMetalMaterial(Materials.invar);
+			invarTC.craftable = false;
+			
+			registerMaterial(invarTC);
 		}
 
-		/*
-		if (Options.ENABLE_LEAD) {
-			registerTinkerMaterial(Materials.lead.getName(), false, true);
+		// Needs the 'Soft' trait
+		if (Options.enableLead) {
+			TCMetalMaterial leadTC = new TCMetalMaterial(Materials.lead);
+			leadTC.craftable = false;
+			
+			registerMaterial(leadTC);
 		}
-		*/
 
 		if (Options.enableMercury) {
 			registerFluid(Materials.mercury, 144);
 		}
 
 		if (Options.enableMithril) {
-			registerMaterial(Materials.mithril, false, true, new TraitHoly());
+			TCMetalMaterial mithrilTC = new TCMetalMaterial(Materials.mithril;
+			mithrilTC.craftable = false;
+			
+			mithrilTC.addTrait(new TraitHoly());
+			registerMaterial(mithrilTC);
+			
 			registerAlloy(Materials.mithril.getName(), 3,
 					new String[] { "silver", "coldiron", "mercury" },
 					new int[] { 2, 1, 1});
 		}
 
 		if (Options.enableNickel) {
-			 registerMaterial(Materials.nickel.getName(), false, true);
+			TCMetalMaterial nickelTC = new TCMetalMaterial(Materials.nickel);
+			nickelTC.craftable = false;
+			
+			registerMaterial(nickelTC);
 		}
 
+		// Maybe give this 'soft' as well ?
 		if (Options.enablePewter) {
-			registerMaterial(Materials.pewter.getName(), false, true);
+			TCMetalMaterial pewterTC = new TCMetalMaterial(Materials.pewter);
+			pewterTC.craftable = false;
+			
+			registerMaterial(pewterTC);
+			// this makes what the "Worshipful Company of Pewterers" called "trifle"
+			registerAlloy(Materials.pewter.getName(), 144,
+					new String[] { "tin", "copper", "lead" },
+					new int[] { 137, 2, 5 } );
 		}
 
 		if (Options.enablePlatinum) {
-			registerMaterial(Materials.platinum.getName(), false, true);
+			TCMetalMaterial platinumTC = new TCMetalMaterial(Materials.platinum);
+			platinumTC.craftable = false;
+			
+			registerMaterial(platinumTC);
 		}
 
+		// Steel has 'Sharp' and 'Stiff'
 		if (Options.enableSteel) {
 			registerAlloy(Materials.steel.fluid.getName(), 8, 
 					new String[] { "iron","coal" },
 					new int[]{8,1});
 		}
 
+		// needs Custom 'sparkly' - autorepair
 		if (Options.enableStarSteel) {
-			registerMaterial(Materials.starsteel.getName(), false, true);
+			TCMetalMaterial starsteelTC = new TCMetalMaterial(Materials.starsteel);
+			starsteelTC.craftable = false;
+			
+			starsteelTC.addTrait(new TraitEnderference(), MaterialTypes.HEAD);
+			registerMaterial(starsteelTC);
 		}
 
 		if (Options.enableTin) {
-			registerMaterial(Materials.tin.getName(), false, true);
+			TCMetalMaterial tinTC = new TCMetalMaterial(Materials.tin);
+			tinTC.craftable = false;
+			
+			registerMaterial(tinTC);
 		}
 
 		if (Options.enableZinc) {
-			registerMaterial(Materials.zinc.getName(), false, true);
+			TCMetalMaterial zincTC = new TCMetalMaterial(Materials.zinc);
+			zincTC.craftable = false;
+			
+			registerMaterial(zincTC);
 		}
 
 		initDone = true;
