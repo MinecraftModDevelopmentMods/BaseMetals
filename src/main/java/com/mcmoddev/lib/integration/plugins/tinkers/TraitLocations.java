5c2ae7039bf005d6a5317ab0d5572f78d110d884
Daniel Hazelton <dshadowwolf@gmail.com>
2017-02-09 21:55:11 -0500

    Lint cleanups and removal of the final direct reference to TiC code from the BMe side of the TiC plugin

---
+++ src/main/java/com/mcmoddev/lib/integration/plugins/tinkers/TraitLocations.java
@@ -0,0 +1,14 @@
+package com.mcmoddev.lib.integration.plugins.tinkers;
+
+public final class TraitLocations {
+	public final String HEAD = "head";
+	public final String HANDLE = "handle";
+	public final String EXTRA = "extra";
+
+	public final String BOW = "bow";
+	public final String BOWSTRING = "bowstring";
+
+	public final String PROJECTILE = "projectile";
+	public final String SHAFT = "shaft";
+	public final String FLETCHING = "fletching";
+}
