acf6014cdf4cc9551c2e54223706cfb64fe47b6f
Daniel Hazelton <dshadowwolf@gmail.com>
2017-02-10 06:20:16 -0500

    Final work on code for TAIGA plugin. TAIGA plugin now functional - needs art and localization

--- src/main/java/com/mcmoddev/basemetals/integration/plugins/TAIGA.java
+++ src/main/java/com/mcmoddev/basemetals/integration/plugins/TAIGA.java
@@ -2,10 +2,9 @@ package com.mcmoddev.basemetals.integration.plugins;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
+import com.mcmoddev.basemetals.BaseMetals;
 import com.mcmoddev.basemetals.integration.BaseMetalsPlugin;
 import com.mcmoddev.lib.integration.IIntegration;
 import com.mcmoddev.lib.integration.plugins.tinkers.TraitRegistry;
@@ -15,6 +14,7 @@ import com.mcmoddev.lib.integration.plugins.taiga.TAIGAItems;
 
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 
 import com.sosnitzka.taiga.Blocks;
 import com.sosnitzka.taiga.Items;
@@ -23,64 +23,55 @@ import com.sosnitzka.taiga.Items;
 public class TAIGA extends com.mcmoddev.lib.integration.plugins.TAIGA implements IIntegration {
 	private static boolean initDone = false;
 
-	private static List<MetalMaterial> myMats = new ArrayList<>();
-	
 	@Override
 	public void init() {
 		if (initDone || !com.mcmoddev.basemetals.util.Config.Options.enableTinkersConstruct) {
 			return;
 		}
 		
-		TraitRegistry.initTAIGATraits();
-		genMetalMaterialsForBlock();
-		TAIGAItems.init(myMats);
+		TraitRegistry.initTAIGATraits();		
+		TAIGAMaterials.init();
+		TAIGAItems.init(TAIGAMaterials.materials);
+		
+		
+//		TraitRegistry.dumpRegistry();
 		
 		initDone = true;
 	}
 	
-	private static void genMetalMaterialsForBlock() {
-		try {
-			Field[] allBlocks = Blocks.class.getDeclaredFields();
-			Field[] allItems = Items.class.getDeclaredFields();
-			Map<String, Item[]> actualItems = new HashMap<>();
-			
-			for( Field i : allItems ) {
-				String t = i.getName();
-				if( !t.startsWith("iron") ) {
-					String name = t.replace("(?:Ingot|Dust|Nugget)", "");
-					if( !actualItems.containsKey(name) ) {
-						Item[] theseItems = new Item[3];
-						theseItems[0] = (Item)Items.class.getField(name+"Ingot").get(Items.class);
-						theseItems[1] = (Item)Items.class.getField(name+"Dust").get(Items.class);
-						theseItems[2] = (Item)Items.class.getField(name+"Nugget").get(Items.class);
-						actualItems.put(name, theseItems);
+	private static class TAIGAMaterials extends com.mcmoddev.lib.init.Materials {
+		private static Field[] allBlocks = Blocks.class.getDeclaredFields();
+		
+		public static List<MetalMaterial> materials = new ArrayList<>();
+		
+		public static void init() {
+			try {
+
+				for( Field f : allBlocks ) {
+					String t = f.getName();
+
+					if( t.endsWith("Block") && t != "basaltBlock" ) {
+						String name = new String(t.substring(0,  t.length() - 5));
+						
+						Block k = (Block) f.get(f.getClass());
+						float harvestlevel = k.getHarvestLevel(null);
+						float resist = k.getExplosionResistance(null);
+
+						MetalMaterial repThis = createOrelessMaterial(name,harvestlevel*3.0f,resist/2.5f,1.0f,0x00000000);
+
+						repThis.block = k;
+						repThis.ingot = new ItemStack( (Item) Items.class.getField(name.toLowerCase()+"Ingot").get(Items.class), 1 ).getItem();
+						repThis.powder = new ItemStack( (Item) Items.class.getField(name.toLowerCase()+"Dust").get(Items.class), 1).getItem();
+						repThis.nugget = new ItemStack( (Item) Items.class.getField(name.toLowerCase()+"Nugget").get(Items.class), 1).getItem();
+						repThis.materialType = MetalMaterial.MaterialType.METAL;
+						
+						materials.add(repThis);
 					}
 				}
+			} catch( final Exception e ) {
+				throw new Error(e.getMessage());
+				// do nought
 			}
-			
-			for( Field f : allBlocks ) {
-				String t = f.getName();
-				if( t.endsWith("Block") && !t.startsWith("basalt") ) {
-					String name = new String(t.substring(0,  t.length() - 5));
-					name = name.substring(0,1).toUpperCase() + name.substring(1);
-
-					Block k = (Block) f.get(f.getClass());
-					float harvestlevel = k.getHarvestLevel(null);
-					float resist = k.getExplosionResistance(null);
-					
-					@SuppressWarnings("deprecation")
-					MetalMaterial repThis = new MetalMaterial(name,harvestlevel*3.0f,resist/2.5f,1.0f,false);
-					
-					repThis.block = k;
-					repThis.ingot = actualItems.get(name.toLowerCase())[0];
-					repThis.powder = actualItems.get(name.toLowerCase())[1];
-					repThis.nugget = actualItems.get(name.toLowerCase())[2];
-					
-					myMats.add(repThis);
-				}
-			}
-		} catch( final Exception e ) {
-			// do nought
 		}
 	}
 }
--- src/main/java/com/mcmoddev/lib/integration/plugins/taiga/TAIGAItems.java
+++ src/main/java/com/mcmoddev/lib/integration/plugins/taiga/TAIGAItems.java
@@ -2,6 +2,7 @@ package com.mcmoddev.lib.integration.plugins.taiga;
 
 import java.util.List;
 
+import com.mcmoddev.basemetals.BaseMetals;
 import com.mcmoddev.lib.init.Items;
 import com.mcmoddev.lib.material.MetalMaterial;
 
@@ -10,6 +11,7 @@ public class TAIGAItems extends Items {
 	
 	public static void init(List<MetalMaterial> materials) {
 		for( MetalMaterial m : materials ) {
+			BaseMetals.logger.error(String.format("BME-TAIGA> calling createItemsFull(%s)", m.getName()));
 			createItemsFull(m);
 		}
 		
