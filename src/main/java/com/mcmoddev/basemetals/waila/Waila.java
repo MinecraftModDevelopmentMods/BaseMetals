ea22ac873b342c5ad63dbf2f4d1073522e5d2bc3
Daniel Hazelton <dshadowwolf@gmail.com>
2017-02-11 09:44:21 -0500

    Implement WAILA (HYWLA) plugin

---
+++ src/main/java/com/mcmoddev/basemetals/waila/Waila.java
@@ -0,0 +1,17 @@
+package com.mcmoddev.basemetals.waila;
+
+import net.minecraftforge.fml.common.event.FMLInterModComms;
+
+import com.mcmoddev.lib.blocks.BlockMetalLever;
+
+import mcp.mobius.waila.api.IWailaRegistrar;
+
+public class Waila {
+	public static void init() {
+		FMLInterModComms.sendMessage("Waila",  "register", "com.mcmoddev.basemetals.waila.register");
+	}
+	
+	public static void register(IWailaRegistrar registrar) {
+		registrar.registerBodyProvider( new leverInfoController(), BlockMetalLever.class);
+	}
+}
