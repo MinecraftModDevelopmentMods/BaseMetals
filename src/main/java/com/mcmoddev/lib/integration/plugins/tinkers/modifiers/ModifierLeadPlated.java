841a6377191ff1e439b1f4b6fbe83ab7fdad3833
Daniel Hazelton <dshadowwolf@gmail.com>
2017-02-14 00:02:46 -0500

    Lay the foundations for custom TiC modifiers

---
+++ src/main/java/com/mcmoddev/lib/integration/plugins/tinkers/modifiers/ModifierLeadPlated.java
@@ -0,0 +1,13 @@
+package com.mcmoddev.lib.integration.plugins.tinkers.modifiers;
+
+import slimeknights.tconstruct.library.modifiers.ModifierTrait;
+
+// We really need nothing here other than to know its been applied.
+
+public class ModifierLeadPlated extends ModifierTrait {
+
+	public ModifierLeadPlated() {
+		super("lead-plated", 0xFFFFFF, 1, 0);
+	}
+
+}
---
+++ src/main/java/com/mcmoddev/lib/integration/plugins/tinkers/modifiers/ModifierToxic.java
@@ -0,0 +1,22 @@
+package com.mcmoddev.lib.integration.plugins.tinkers.modifiers;
+
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.init.MobEffects;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.PotionEffect;
+import slimeknights.tconstruct.library.modifiers.ModifierTrait;
+
+public class ModifierToxic extends ModifierTrait {
+
+	public ModifierToxic() {
+		super("toxic-modifier", 0xFFFFFF, 1, 0);
+	}
+	
+	@Override
+	public void afterHit(ItemStack tool, EntityLivingBase player, EntityLivingBase target, float damageDealt, boolean wasCritical, boolean wasHit) {
+		if( wasHit && target.isEntityAlive()) {
+			target.addPotionEffect(new PotionEffect(MobEffects.POISON, 50, 1));
+			target.addPotionEffect(new PotionEffect(MobEffects.BLINDNESS, 10 ));
+		}
+	}
+}
