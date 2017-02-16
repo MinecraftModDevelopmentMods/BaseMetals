3af30f702d28802f533a8d02c8dd71bdcf46fbbd
Daniel Hazelton <dshadowwolf@gmail.com>
2017-02-16 15:36:30 -0500

    add interface helpers for NMe and EMe registering with the DenseOres plugin

--- src/main/java/com/mcmoddev/lib/integration/plugins/DenseOres.java
+++ src/main/java/com/mcmoddev/lib/integration/plugins/DenseOres.java
@@ -31,7 +31,6 @@ public class DenseOres implements IIntegration {
 	 * @author Daniel Hazelton &lt;dshadowwolf@gmail.com&gt;
 	 */
 	public static void registerOre(String name, String modID, String underlying, int meta) {
-		BaseMetals.logger.error("Registering %s, baseBlock %s:%s with meta %d and baseBlockTexture blocks/%s", name, modID, name, meta, underlying);
 		NBTTagCompound mess = new NBTTagCompound();
 		mess.setString("baseBlock", String.format("%s:%s", modID, name));
 		mess.setInteger("baseBlockMeta", meta);
@@ -41,7 +40,30 @@ public class DenseOres implements IIntegration {
 		// this, however, is apparently a name, so...
 		mess.setString("config_entry", String.format("%s %s ore", modID, name));
 		
-		BaseMetals.logger.error("Sending Message for material %s with baseBlock set to %s:%s and meta of %d", name, modID, name, meta);
 		FMLInterModComms.sendMessage("denseores",  "addDenseOre", mess);
+	}
+
+	/**
+	 * Register a single ore that spawns in the nether and has an oredict of 'nether_&lt;name&gt;_ore'
+	 *
+	 * @param name Oredictionary name of this ore in its source mod
+	 * @param modID ID of mod that is the source of this ore
+	 * @param meta The metadata value for this ore in the source block
+	 * @author Daniel Hazelton &lt;dshadowwolf@gmail.com&gt;
+	 */
+	public static void registerNetherOre( String name, String modID, int meta ) {
+		registerOre( String.format("nether_%s", name), modID, "netherrack", meta );
+	}
+
+	/**
+	 * Register a single ore that spawns in the end and has an oredict of 'end_&lt;name&gt;_ore'
+	 *
+	 * @param name Oredictionary name of this ore in its source mod
+	 * @param modID ID of mod that is the source of this ore
+	 * @param meta The metadata value for this ore in the source block
+	 * @author Daniel Hazelton &lt;dshadowwolf@gmail.com&gt;
+	 */
+	public static void registerEndOre( String name, String modID, int meta ) {
+		registerOre( String.format("end_%s", name), modID, "end_stone", meta );
 	}
 }
