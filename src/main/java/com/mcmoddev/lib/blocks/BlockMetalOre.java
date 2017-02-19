8191538930ad4b35fae6625ab6a81853f1f91f87
Daniel Hazelton <dshadowwolf@gmail.com>
2017-02-19 17:30:19 -0500

    Minerals drop powders, Gems drop gems other ores drop themselves - this sets that up and also implements a fortune enchant handler and a touch of randomness for Mineral and Gem drops

--- src/main/java/com/mcmoddev/lib/blocks/BlockMetalOre.java
+++ src/main/java/com/mcmoddev/lib/blocks/BlockMetalOre.java
@@ -10,8 +10,12 @@ import net.minecraft.block.BlockOre;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
+
+import java.util.Random;
 
 /**
  * Ore Block
@@ -27,7 +31,7 @@ public class BlockMetalOre extends BlockOre implements IOreDictionaryEntry, IMet
 	 */
 	public BlockMetalOre(MetalMaterial material) {
 		super();
-		this.setSoundType(SoundType.STONE);
+//		this.setSoundType(SoundType.STONE);
 		this.material = material;
 		this.blockHardness = Math.max(5f, material.getOreBlockHardness());
 		this.blockResistance = Math.max(1.5f, material.getBlastResistance() * 0.75f);
@@ -69,5 +73,32 @@ public class BlockMetalOre extends BlockOre implements IOreDictionaryEntry, IMet
 	@Deprecated
 	public MetalMaterial getMetalMaterial() {
 		return this.material;
+	}
+
+	@Override
+	public int quantityDropped(IBlockState state, int fortune, Random random) {
+		int most = 1;
+		int least = 1;
+		int total;
+		switch( this.material.getType() ) {
+			case MINERAL:
+				most = 4;
+				least = 2;
+				break;
+			case GEM:
+				most = 3;
+				least = 2;
+				break;
+		}
+		total = ((most - least) + fortune) + 1;
+		return least + random.nextInt(total);
+	}
+
+	@Override
+	public Item getItemDropped(IBlockState state, Random random, int fortune) {
+		if( this.material.getType() == MetalMaterial.MaterialType.MINERAL ) {
+			return this.material.powder;
+		}
+		return this.material.ingot;
 	}
 }
