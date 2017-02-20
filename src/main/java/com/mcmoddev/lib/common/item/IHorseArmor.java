6dcc5cca6abf13dbeec0662ceb33b1a96ed699c5
Jasmine Iwanek <jasmine@iwanek.co.uk>
2017-02-20 02:17:27 -0800

    cleaning in common

--- src/main/java/com/mcmoddev/lib/common/item/IHorseArmor.java
+++ src/main/java/com/mcmoddev/lib/common/item/IHorseArmor.java
@@ -6,21 +6,23 @@ import net.minecraft.item.ItemStack;
 
 public interface IHorseArmor {
 
-    /**
-     * Returns the {@link HorseArmorType} of the custom horse armor.
-     *
-     * @return The {@link HorseArmorType} that this horse armor will have the values of.
-     */
-    HorseArmorType getArmorType();
+	/**
+	 * Returns the {@link HorseArmorType} of the custom horse armor.
+	 *
+	 * @return The {@link HorseArmorType} that this horse armor will have the values of.
+	 */
+	HorseArmorType getArmorType();
 
-    /**
-     * Returns the location of the custom horse armor texture, similar to how player armor
-     * texture works.
-     *
-     * @param horse The Horse, which has this item equipped
-     * @param stack The ItemStack instance of this item from the Horse
-     *
-     * @return The location of the custom horse armor
-     */
-    String getArmorTexture (EntityHorse horse, ItemStack stack);
+	/**
+	 * Returns the location of the custom horse armor texture, similar to how
+	 * player armor texture works.
+	 *
+	 * @param horse
+	 *            The Horse, which has this item equipped
+	 * @param stack
+	 *            The ItemStack instance of this item from the Horse
+	 *
+	 * @return The location of the custom horse armor
+	 */
+	String getArmorTexture(EntityHorse horse, ItemStack stack);
 }
