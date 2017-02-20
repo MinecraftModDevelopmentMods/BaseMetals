f7b016fbd3b6f01b9a57b501b052857dd02c3686
Jasmine Iwanek <jasmine@iwanek.co.uk>
2017-02-20 02:08:11 -0800

    missed one

--- src/main/java/com/mcmoddev/lib/integration/plugins/tinkers/TCMetalMaterial.java
+++ src/main/java/com/mcmoddev/lib/integration/plugins/tinkers/TCMetalMaterial.java
@@ -19,8 +19,8 @@ import slimeknights.tconstruct.library.traits.ITrait;
  * @author Daniel Hazelton &lt;dshadowwolf@gmail.com&gt;
  * @since 8-FEB-2017
  * 
- * Wrapper for all the data that the TiC plugin needs to generate a TiC material
- * and tell TiC how the various parts are to act.
+ *        Wrapper for all the data that the TiC plugin needs to generate a TiC
+ *        material and tell TiC how the various parts are to act.
  */
 
 public class TCMetalMaterial {
@@ -49,7 +49,7 @@ public class TCMetalMaterial {
 	// Because some *metals need to change this
 	// this is in mB
 	public int amountPerOre = 288;
-	
+
 	// for reference and simplifying the API
 	public final MetalMaterial metalmaterial;
 
@@ -60,10 +60,11 @@ public class TCMetalMaterial {
 
 	public boolean hasTraits;
 
-	private HashMap<String,List<AbstractTrait>> traits = new HashMap<>();
+	private HashMap<String, List<AbstractTrait>> traits = new HashMap<>();
 
 	/**
-	 * @param material MetalMaterial this represents
+	 * @param material
+	 *            MetalMaterial this represents
 	 */
 	public TCMetalMaterial(MetalMaterial material) {
 		headDurability = material.getToolDurability();
@@ -106,32 +107,38 @@ public class TCMetalMaterial {
 
 	/**
 	 * Add a TiC default or custom trait to the material in general
-	 * @param trait the AbstractTrait to add
+	 * 
+	 * @param traitName
+	 *            the AbstractTrait to add
 	 */
 	public void addTrait(String traitName) {
 		addTrait(traitName, "general");
 	}
 
 	/**
-	 * Add a TiC default or custom trait to the material when used as a specific tool part
-	 * @param trait the AbstractTrait to add
-	 * @param loc the MaterialType for the tool part {@link slimeknights.tconstruct.library.material.MaterialType}
+	 * Add a TiC default or custom trait to the material when used as a specific
+	 * tool part
+	 * 
+	 * @param traitName the AbstractTrait to add
+	 * @param loc the MaterialType for the tool part
+	 * {@link slimeknights.tconstruct.library.material.MaterialType}
+	 * 
 	 * @throws
 	 */
 	public void addTrait(String traitName, String loc) {
 		ITrait trait = TraitRegistry.get(traitName);
-		if( trait == null ) {
+		if (trait == null) {
 			String s = String.format("BaseMetals-TCon: Asked for trait %s that does not exist", traitName);
 			BaseMetals.logger.error(s);
 			return;
 		}
 		if (traits.keySet().contains(loc)) {
-			if (!traits.get(loc).add((AbstractTrait)trait)) { 
+			if (!traits.get(loc).add((AbstractTrait) trait)) {
 				throw new RuntimeException("Unable to add trait!");
 			}
 		} else {
 			List<AbstractTrait> t = new ArrayList<>();
-			t.add((AbstractTrait)trait);
+			t.add((AbstractTrait) trait);
 			traits.put(loc, t);
 		}
 
@@ -140,8 +147,11 @@ public class TCMetalMaterial {
 
 	/**
 	 * Get the trait for a given location
+	 * 
 	 * @param loc the MaterialType for the tool part of the trait, null for the overall one
+	 * 
 	 * @return the instance of the trait that was originally stored
+	 * 
 	 * @throws
 	 */
 	public List<AbstractTrait> getTraits(String loc) {
@@ -149,7 +159,7 @@ public class TCMetalMaterial {
 			return new ArrayList<>();
 		}
 
-		if( loc == null ) {
+		if (loc == null) {
 			return traits.get("general");
 		} else if (traits.keySet().contains(loc)) {
 			return traits.get(loc);
@@ -157,9 +167,10 @@ public class TCMetalMaterial {
 			throw new RuntimeException("Unkown trait location " + loc);
 		}
 	}
-	
+
 	/**
 	 * Get the names of the keys in the traits HashMap
+	 * 
 	 * @return Set<String>
 	 */
 	public Set<String> getTraitLocs() {
