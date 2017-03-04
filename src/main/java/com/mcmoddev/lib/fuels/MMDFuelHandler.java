eac0542c0296af9e41e7899e2253da03dbb9f028
Daniel Hazelton <dshadowwolf@gmail.com>
2017-03-04 00:42:28 -0500

    add an API for registering new fuels for the furnace and any machines which use the vanilla fuels system

---
+++ src/main/java/com/mcmoddev/lib/fuels/MMDFuelHandler.java
@@ -0,0 +1,20 @@
+package com.mcmoddev.lib.fuels;
+
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.common.IFuelHandler;
+
+class MMDFuelHandler implements IFuelHandler {
+	
+	public MMDFuelHandler() {
+	}
+	
+	@Override
+	public int getBurnTime(ItemStack fuel) {
+		if( FuelRegistry.getFuels().containsKey(fuel.getItem()) ) {
+			return FuelRegistry.getFuels().get(fuel.getItem());
+		}
+		
+		return 0;
+	}
+	
+}
