f65cf724cb7c548f95f26a94bee5c31c0d7f0841
Daniel Hazelton <dshadowwolf@gmail.com>
2017-06-13 22:16:17 -0400

    fix the JEI plugin

--- src/main/java/com/mcmoddev/lib/jei/CrusherRecipeCategory.java
+++ src/main/java/com/mcmoddev/lib/jei/CrusherRecipeCategory.java
@@ -3,81 +3,76 @@ package com.mcmoddev.lib.jei;
 import java.util.List;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 import mezz.jei.api.IGuiHelper;
+import mezz.jei.api.gui.ICraftingGridHelper;
 import mezz.jei.api.gui.IDrawable;
 import mezz.jei.api.gui.IDrawableAnimated;
 import mezz.jei.api.gui.IDrawableStatic;
+import mezz.jei.api.gui.IGuiItemStackGroup;
 import mezz.jei.api.gui.IRecipeLayout;
 import mezz.jei.api.ingredients.IIngredients;
-import mezz.jei.api.recipe.IRecipeCategory;
+import mezz.jei.api.recipe.BlankRecipeCategory;
 import mezz.jei.api.recipe.IRecipeWrapper;
-import net.minecraft.client.Minecraft;
+import mezz.jei.util.Translator;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.text.translation.I18n;
 
-@SuppressWarnings({ "rawtypes", "deprecation" })
-public class CrusherRecipeCategory implements IRecipeCategory {
+public class CrusherRecipeCategory extends BlankRecipeCategory<IRecipeWrapper> {
 
 	private final ResourceLocation resourceLocation = new ResourceLocation("basemetals", "textures/jei/JEIhammeroverlay.png");
 	private final IDrawable background;
 	private final IDrawable icon;
 	protected final IDrawableAnimated hammer;
-
+	private final ICraftingGridHelper craftingGridHelper;
+	
 	public CrusherRecipeCategory(IGuiHelper guiHelper) {
 		background = guiHelper.createDrawable(resourceLocation, 0, 0, 166, 130);
 		icon = guiHelper.createDrawable(resourceLocation, 170, 2, 16, 16);
 
 		IDrawableStatic hammerDrawable = guiHelper.createDrawable(resourceLocation, 169, 17, 32, 32);
 		hammer = guiHelper.createAnimatedDrawable(hammerDrawable, 200, IDrawableAnimated.StartDirection.BOTTOM, false);
+		craftingGridHelper = guiHelper.createCraftingGridHelper(0, 1);
 	}
 
-	@Nonnull
 	@Override
 	public String getUid() {
 		return BaseMetalsJEIPlugin.JEIUID + ".crackhammer";
 	}
 
-	@Nonnull
 	@Override
 	public String getTitle() {
-		return I18n.translateToLocalFormatted(getUid());
+		return Translator.translateToLocal(getUid());
 	}
 
-	@Nonnull
 	@Override
 	public IDrawable getBackground() {
 		return background;
 	}
 
-	@Nonnull
+	@Nullable
 	@Override
 	public IDrawable getIcon() {
 		return icon;
 	}
 
 	@Override
-	public void drawExtras(Minecraft minecraft) {
-		// TODO - STUB
-	}
-
-	@Override
 	public void setRecipe(IRecipeLayout recipeLayout, @Nonnull IRecipeWrapper recipeWrapper, IIngredients ingredients) {
-		// custom tooltips
-		recipeLayout.getItemStacks().addTooltipCallback((CrusherRecipeWrapper) recipeWrapper);
+		IGuiItemStackGroup guiItemStacks = recipeLayout.getItemStacks();
+		
+		// init the input slot
+		guiItemStacks.init(0, true, 40, 14);
+		
+		// init the output slot
+		guiItemStacks.init(1, false, 119, 14);
 
-		// input
-		recipeLayout.getItemStacks().init(0, true, 40, 14);
-		recipeLayout.getItemStacks().set(0, (ItemStack) recipeWrapper.getInputs().get(0));
-
-		// output
-		recipeLayout.getItemStacks().init(2, false, 119, 14);
-		recipeLayout.getItemStacks().set(2, (ItemStack) recipeWrapper.getOutputs().get(0));
-	}
-
-	@Override
-	public List getTooltipStrings(int mouseX, int mouseY) {
-		return null;
+		// load the output and input bits
+		List<List<ItemStack>> inputs = ingredients.getInputs(ItemStack.class);
+		List<List<ItemStack>> outputs = ingredients.getOutputs(ItemStack.class);
+		
+		// setup the data
+		craftingGridHelper.setInputs(guiItemStacks, inputs);
+		guiItemStacks.set(1, outputs.get(0));
 	}
 }
--- src/main/java/com/mcmoddev/lib/jei/CrusherRecipeHandler.java
+++ src/main/java/com/mcmoddev/lib/jei/CrusherRecipeHandler.java
@@ -5,32 +5,34 @@ import javax.annotation.Nonnull;
 import mezz.jei.api.recipe.IRecipeHandler;
 import mezz.jei.api.recipe.IRecipeWrapper;
 
+import com.mcmoddev.lib.registry.recipe.ICrusherRecipe;
+
 /**
  *
  * @author DrCyano
  * @author Daniel Hazelton
  *
  */
-public class CrusherRecipeHandler implements IRecipeHandler<CrusherRecipeWrapper> {
+public class CrusherRecipeHandler implements IRecipeHandler<ICrusherRecipe> {
 
 	@Override
-	public Class<CrusherRecipeWrapper> getRecipeClass() {
-		return CrusherRecipeWrapper.class;
+	public Class<ICrusherRecipe> getRecipeClass() {
+		return ICrusherRecipe.class;
 	}
 
 	@Nonnull
 	@Override
-	public String getRecipeCategoryUid(@Nonnull CrusherRecipeWrapper recipe) {
+	public String getRecipeCategoryUid(@Nonnull ICrusherRecipe recipe) {
 		return BaseMetalsJEIPlugin.JEIUID + ".crackhammer";
 	}
 
 	@Override
-	public IRecipeWrapper getRecipeWrapper(CrusherRecipeWrapper recipe) {
-		return recipe;
+	public IRecipeWrapper getRecipeWrapper(ICrusherRecipe recipe) {
+		return new CrusherRecipeWrapper(recipe);
 	}
 
 	@Override
-	public boolean isRecipeValid(CrusherRecipeWrapper recipe) {
-		return !(recipe.getInputs().isEmpty() || recipe.getOutputs().isEmpty());
+	public boolean isRecipeValid(ICrusherRecipe recipe) {
+		return true;
 	}
 }
