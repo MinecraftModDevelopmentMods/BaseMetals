0ab1a3469e2eda1d74b24639afacf7d352524c30
Daniel Hazelton <dshadowwolf@gmail.com>
2017-06-14 00:29:09 -0400

    At some point OPTIONAL_ITEM_STACK lost the Optional qualifier - copy from the stable_29 tree to get this working again

--- src/main/java/com/mcmoddev/lib/asm/ASMHooks.java
+++ src/main/java/com/mcmoddev/lib/asm/ASMHooks.java
@@ -1,20 +1,41 @@
 package com.mcmoddev.lib.asm;
 
+import java.io.IOException;
+
 import com.google.common.base.Optional;
 import com.mcmoddev.lib.common.item.IHorseArmor;
 
 import net.minecraft.entity.passive.EntityHorse;
 import net.minecraft.entity.passive.HorseArmorType;
 import net.minecraft.item.ItemStack;
+import net.minecraft.network.PacketBuffer;
 import net.minecraft.network.datasync.DataParameter;
-import net.minecraft.network.datasync.DataSerializers;
+import net.minecraft.network.datasync.DataSerializer;
 import net.minecraft.network.datasync.EntityDataManager;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class ASMHooks {
 
-	public static final DataParameter<Optional<ItemStack>> ARMOR_STACK = EntityDataManager.createKey(EntityHorse.class, DataSerializers.OPTIONAL_ITEM_STACK);
+	private static final DataSerializer<Optional<ItemStack>> OPTIONAL_ITEM_STACK = new DataSerializer<Optional<ItemStack>>() {
+		@Override
+        public void write(PacketBuffer buf, Optional<ItemStack> value)
+        {
+			buf.writeItemStack((ItemStack)value.orNull());
+        }
+		@Override
+        public Optional<ItemStack> read(PacketBuffer buf) throws IOException
+        {
+            return Optional.<ItemStack>fromNullable(buf.readItemStack());
+        }
+		@Override
+        public DataParameter<Optional<ItemStack>> createKey(int id)
+        {
+            return new DataParameter<Optional<ItemStack>>(id, this);
+        }
+    };
+    
+	public static final DataParameter<Optional<ItemStack>> ARMOR_STACK = EntityDataManager.createKey(EntityHorse.class, OPTIONAL_ITEM_STACK);
 
 	private ASMHooks() {
 		throw new IllegalAccessError("Not a instantiable class");
