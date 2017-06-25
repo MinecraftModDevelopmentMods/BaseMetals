be04b39e0fbfa7cecdf40473bb773a579f9bb9ad
Daniel Hazelton <dshadowwolf@gmail.com>
2017-06-24 23:55:33 -0500

    some work on making the JEI integration for the crackhammer/ICrusherRecipe stuff work again - seems the animation isn't working, sadly

--- src/main/java/com/mcmoddev/lib/jei/BaseMetalsJEIPlugin.java
+++ src/main/java/com/mcmoddev/lib/jei/BaseMetalsJEIPlugin.java
@@ -1,16 +1,23 @@
 package com.mcmoddev.lib.jei;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 
 import com.mcmoddev.basemetals.BaseMetals;
 import com.mcmoddev.lib.registry.CrusherRecipeRegistry;
+import com.mcmoddev.lib.registry.recipe.ArbitraryCrusherRecipe;
 import com.mcmoddev.lib.registry.recipe.ICrusherRecipe;
+import com.mcmoddev.lib.registry.recipe.OreDictionaryCrusherRecipe;
 
 import mezz.jei.api.BlankModPlugin;
 import mezz.jei.api.IGuiHelper;
 import mezz.jei.api.IJeiHelpers;
 import mezz.jei.api.IModRegistry;
 import mezz.jei.api.JEIPlugin;
+import mezz.jei.api.recipe.IRecipeCategoryRegistration;
+import mezz.jei.api.recipe.IRecipeWrapper;
+import mezz.jei.api.recipe.IRecipeWrapperFactory;
 
 /**
  *
@@ -22,17 +29,25 @@ import mezz.jei.api.JEIPlugin;
 public class BaseMetalsJEIPlugin extends BlankModPlugin {
 
 	public static final String JEIUID = BaseMetals.MODID;
+	public static final String recipeUID = JEIUID + ".crackhammer";
+
+	@Override
+	public void registerCategories(IRecipeCategoryRegistration registry) {
+		final IGuiHelper guiHelper = registry.getJeiHelpers().getGuiHelper();
+
+		registry.addRecipeCategories(new ICrusherRecipeCategory(guiHelper));
+	}
+
 
 	@Override
 	public void register(IModRegistry registry) {
+		registry.addRecipes(CrusherRecipeRegistry.getInstance().getAllRecipes().stream().map((ICrusherRecipe in) -> new ICrusherRecipeWrapper(in)).collect(Collectors.toList()), recipeUID);
 
-		IJeiHelpers jeiHelpers = registry.getJeiHelpers();
-		IGuiHelper guiHelper = jeiHelpers.getGuiHelper();
-
-		registry.addRecipeCategories(new CrusherRecipeCategory(guiHelper));
-		registry.addRecipeHandlers(new CrusherRecipeHandler());
-
-		registry.addRecipes(CrusherRecipeRegistry.getInstance().getAllRecipes().stream().map((ICrusherRecipe in) -> new CrusherRecipeWrapper(in)).collect(Collectors.toList()));
-
+		registry.handleRecipes(ICrusherRecipe.class, new IRecipeWrapperFactory<ICrusherRecipe>() {
+			@Override
+			public IRecipeWrapper getRecipeWrapper(ICrusherRecipe recipe) {
+				return new ICrusherRecipeWrapper(recipe);
+			}
+		}, recipeUID);		
 	}
 }
---
+++ src/main/java/com/mcmoddev/lib/jei/ICrusherRecipeWrapper.java
@@ -0,0 +1,38 @@
+package com.mcmoddev.lib.jei;
+
+import java.util.Collections;
+import java.util.List;
+
+import com.mcmoddev.lib.registry.recipe.ICrusherRecipe;
+
+import mezz.jei.api.ingredients.IIngredients;
+import mezz.jei.api.recipe.BlankRecipeWrapper;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.item.ItemStack;
+
+public class ICrusherRecipeWrapper extends BlankRecipeWrapper {
+
+	private ICrusherRecipe theRecipe;
+	
+	public ICrusherRecipeWrapper(ICrusherRecipe recipe) {
+		this.theRecipe = recipe;
+	}
+
+	@Override
+	public void getIngredients(IIngredients ingredients) {
+		ingredients.setInputs(ItemStack.class, theRecipe.getInputs());
+		ingredients.setOutput(ItemStack.class, theRecipe.getOutput());
+	}
+
+	@Override
+	public void drawInfo(Minecraft minecraft, int recipeWidth, int recipeHeight, int mouseX, int mouseY) {
+		// add the recipes type at the top of the screen
+		minecraft.fontRenderer.drawString(I18n.format("basemetals.description.crackhammer"),43, 0, 0);
+	}
+
+	@Override
+	public List<String> getTooltipStrings(int mouseX, int mouseY) {
+		return Collections.emptyList();
+	}
+}
