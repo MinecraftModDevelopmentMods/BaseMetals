c65081770cb9d7576a5069822e7b0d381bbadf00
Jasmine Iwanek <jasmine@iwanek.co.uk>
2017-07-13 17:48:06 -0700

    Fixes.

--- src/main/java/com/mcmoddev/alt/BaseMetalsALT.java
+++ src/main/java/com/mcmoddev/alt/BaseMetalsALT.java
@@ -3,6 +3,6 @@ import com.mcmoddev.alt.api.IALTPlugin;
 import com.mcmoddev.alt.api.ALTPlugin;
 
 @ALTPlugin(modid = "basemetals")
-class MyALTPlugin implements IALTPlugin {
+class BaseMetalsALT implements IALTPlugin {
   // Intentionally left blank
 }
