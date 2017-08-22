3cc605f6ee78583ba98a328be045edd6bb6015bc
LakMoore <stuart@equipmyschool.com>
2017-08-22 09:16:01 +0100

    Just wanted to fix this exception.

--- src/main/java/com/mcmoddev/lib/asm/ASMTransformer.java
+++ src/main/java/com/mcmoddev/lib/asm/ASMTransformer.java
@@ -33,6 +33,9 @@ public class ASMTransformer implements IClassTransformer {
 	}
 
 	private void saveBytecode(String name, ClassWriter cw) {
+	    //this checks whether we are in a de-obfuscated environment
+	    //but we are calling it in an ASM transformer running 
+	    //@SortingIndex(1001) so it will always be de-obfuscated
 		if (Platform.isDevEnv()) {
 			File output = null; 
 			FileOutputStream out = null;
@@ -40,9 +43,9 @@ public class ASMTransformer implements IClassTransformer {
 				final File debugDir = new File("mmd/asm/debug/");
 				if (debugDir.exists())
 					debugDir.delete();
+                debugDir.mkdirs();  //moved this as next line was throwing an exception on first execution
 				output = new File(debugDir, name + ".class");
 				out = new FileOutputStream(output);
-				debugDir.mkdirs();
 				out.write(cw.toByteArray());
 			} catch (final IOException ex) {
 				BaseMetals.logger.error(ex);
