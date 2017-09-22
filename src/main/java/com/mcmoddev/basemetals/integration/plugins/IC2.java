7be67f42becebb7a0bc97fc862b524283d784a2a
dshadowwolf <dshadowwolf@gmail.com>
2017-09-22 01:31:11 -0400

    Master 1.12 dev (#182)
    * fix the logic bug in the crossbow recipe - its "CrossbowAndBolt" not "CrossbowAndBolts" (never code while tired, kids!)
    
    * screwed up that last commit badly, lets try this again
    
    * fix a mistake that keeps creeping in of some of the jsons having single-quotes around some elements instead of double-quotes
    
    * fixed the outstanding bugs in the json's that crept in somewhere along the line
    
    * it's Trapdoor, not Trapdoors for that one
    
    * more recipe fixes
    
    * mithril somehow got missed for almost everything until now - thats been fixed
    
    * fix some recurring issues thanks to the code being a touch crufty and wrong
    
    * fix some stuff up, this fixes JEI - though Windows is screwing me with it being case-insensitive... someone on a *nix machine has to move the textures/jei/JEIHammerOverlay.png to lowercase
    
    * cleanups and work on trying to make code a touch more robust and the plugins functional
    
    * some more cleanups and a touch of magic that might make the TiCon plugin start working again
    
    * ...blargh...
    
    * make the TiCon plugin work - thanks to the TiCon crew (boni and KnightMiner, you da best!) for making the API easier to use
    
    * recipe fix run - this should have fixed the names to remove camel-case in material names for ore-dict except the initial caps on the actual name of the material
    
    * re-apply the fix to the bug in the script
    
    * Fix the registration system of TiCon since the changes that the TiCon crew made to the API internals have changed how the reg system works - this should avoid multi-reg
    
    * cleanups and re-enabling Mithril - forgot that in the original push of fixing the TiCon plugin. Yes, dshad made a dummy mistake
    
    * update to Mek 9 and make the plugin work as close to correctly as possible without setting up the entire processing system
    
    * fix the IC2 plugin, make it work and wire in Adamantine as a test - need to move on to other plugins after this, so someone else can finish adding the materials
    
    * Most of this is a complete replacement of the CrusherRecipeRegistry with an IForgeRegistry implementation. Cuts the code size by about half, and most of that is actually back-compat.
    
    The rest is poking at the IC2 plugin, just to find that there is some funkiness going on in the MMDMaterial internal block/item registry. Sorry, folks!
    
    More CrusherRecipeRegistry work later, when the selection of parameter types for removing crusher recipes becomes available.
    
    * implement the removal of recipes from the CrusherRecipeRegistry
    
    * cleanup, some code re-org
    
    * Fix the Tinkers' plugin - fixes #179 and some issues that had not been noticed due to the TiCon API not having visible external changes
    
    * Update the Materials registry to an IForgeRegistry system instead of the homebrewed one we've been using
    
    * update this to match the new internal names
    
    * co-opt the recipe registry event so this runs after the items and blocks are registered with the system, but before init - the materials coming in with 'Block{null}' and causing IC2 to think its trying to make Air seems to be caused by an issue in this area
    
    * be a touch pedantic and make sure that the ItemBlock has the exact same registry name as the Block
    
    * shift this to using a ConcurrentHashMap instead of a TreeMap
    
    * fix an ore-dict issue with the small-powder and small-blend naming, though there seems to be an issue outside the registry code in this area

--- src/main/java/com/mcmoddev/basemetals/integration/plugins/IC2.java
+++ src/main/java/com/mcmoddev/basemetals/integration/plugins/IC2.java
@@ -5,27 +5,37 @@ import com.mcmoddev.basemetals.data.MaterialNames;
 import com.mcmoddev.lib.integration.IIntegration;
 import com.mcmoddev.lib.integration.MMDPlugin;
 import com.mcmoddev.lib.util.ConfigBase.Options;
+
+import net.minecraft.item.crafting.IRecipe;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
 import com.mcmoddev.lib.integration.plugins.IC2Base;
 
 @MMDPlugin(addonId = BaseMetals.MODID, pluginId = IC2.PLUGIN_MODID)
 public class IC2 extends IC2Base implements IIntegration {
 
 	private static boolean initDone = false;
-
+	
 	@Override
 	public void init() {
 		if (initDone || !Options.isModEnabled(IC2.PLUGIN_MODID)) {
 			return;
 		}
-		
-/*  examples! */		
+
+		MinecraftForge.EVENT_BUS.register(this);
+		initDone = true;
+	}
+	
+	@SubscribeEvent
+	public void regCallback(RegistryEvent.Register<IRecipe> ev) {
+		/*  examples! */		
 		registerVanillaRecipes(MaterialNames.ADAMANTINE);
 		addMaceratorRecipes(MaterialNames.ADAMANTINE);
 		addOreWashingPlantRecipes(MaterialNames.ADAMANTINE);
 		addThermalCentrifugeRecipes(MaterialNames.ADAMANTINE);
 		// broken, somehow
-		// addMetalFormerRecipes(MaterialNames.ADAMANTINE);
-		
-		initDone = true;
+		addMetalFormerRecipes(MaterialNames.ADAMANTINE);
 	}
 }
