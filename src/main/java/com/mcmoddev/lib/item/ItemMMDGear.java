30cce29103908f81392fb32eb885759f5095a9c8
Daniel "DShadowWolf" Hazelton <dshadowwolf@gmail.com>
2018-01-02 16:32:16 -0600

    Vanilla appears to expect all Material.WOOD items to be fuels and is complaining that the MMD Wooden Gear does not have a set burn time - making it a non-fuel. This corrects that and should also fix #249

--- src/main/java/com/mcmoddev/lib/item/ItemMMDGear.java
+++ src/main/java/com/mcmoddev/lib/item/ItemMMDGear.java
@@ -1,5 +1,7 @@
 package com.mcmoddev.lib.item;
 
+import com.mcmoddev.basemetals.data.MaterialNames;
+import com.mcmoddev.basemetals.init.Materials;
 import com.mcmoddev.lib.material.MMDMaterial;
 
 /**
@@ -17,5 +19,10 @@ public class ItemMMDGear extends GenericMMDItem {
 	 */
 	public ItemMMDGear(MMDMaterial material) {
 		super(material);
+		if (material.equals(Materials.getMaterialByName(MaterialNames.WOOD))) {
+			setBurnTime(300); // Coal has 80 second burn, doing 8 items - that's 10s/it or 200t/it - Planks
+								// are reported at 1.5 items per plank, ie: 300 ticks. So we're using that
+								// as the burn-time for gears
+		}
 	}
 }
