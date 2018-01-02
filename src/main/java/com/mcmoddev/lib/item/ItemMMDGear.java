de22b7ba925c877a592d704d33af1f64e8bb7b5b
Daniel "DShadowWolf" Hazelton <dshadowwolf@gmail.com>
2018-01-02 16:54:47 -0600

    fix this to use the proper generics

--- src/main/java/com/mcmoddev/lib/item/ItemMMDGear.java
+++ src/main/java/com/mcmoddev/lib/item/ItemMMDGear.java
@@ -1,8 +1,7 @@
 package com.mcmoddev.lib.item;
 
-import com.mcmoddev.basemetals.data.MaterialNames;
-import com.mcmoddev.basemetals.init.Materials;
 import com.mcmoddev.lib.material.MMDMaterial;
+import com.mcmoddev.lib.material.MMDMaterial.MaterialType;
 
 /**
  * Gears
@@ -19,7 +18,7 @@ public class ItemMMDGear extends GenericMMDItem {
 	 */
 	public ItemMMDGear(MMDMaterial material) {
 		super(material);
-		if (material.equals(Materials.getMaterialByName(MaterialNames.WOOD))) {
+		if (material.getType().equals(MaterialType.WOOD)) {
 			setBurnTime(300); // Coal has 80 second burn, doing 8 items - that's 10s/it or 200t/it - Planks
 								// are reported at 1.5 items per plank, ie: 300 ticks. So we're using that
 								// as the burn-time for gears
