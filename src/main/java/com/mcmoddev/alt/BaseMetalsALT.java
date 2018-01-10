3faf9de1a161cfbbed0c07339251eaa24b80b3cf
Jasmine Iwanek <jasmine@iwanek.co.uk>
2018-01-09 23:47:46 -0800

    Get the initial support for OS3 in place

---
+++ src/main/java/com/mcmoddev/alt/BaseMetalsALT.java
@@ -0,0 +1,9 @@
+package com.mcmoddev.alt;
+
+import com.mcmoddev.alt.api.ALTPlugin;
+import com.mcmoddev.alt.api.IALTPlugin;
+
+@ALTPlugin(modid = "basemetals")
+class BaseMetalsALT implements IALTPlugin {
+	// Intentionally left blank
+}
