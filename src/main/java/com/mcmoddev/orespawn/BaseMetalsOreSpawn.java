438e854cab2d00b9e2e48ad6365b1d9f50ccb585
Jasmine Iwanek <jasmine@iwanek.co.uk>
2018-01-09 21:29:09 -0800

    Get newer ALT and OS support code in place (Incomplete)

---
+++ src/main/java/com/mcmoddev/orespawn/BaseMetalsOreSpawn.java
@@ -0,0 +1,15 @@
+package com.mcmoddev.orespawn;
+
+import com.mcmoddev.orespawn.api.os3.OS3API;
+import com.mcmoddev.orespawn.api.plugin.IOreSpawnPlugin;
+import com.mcmoddev.orespawn.api.plugin.OreSpawnPlugin;
+
+@OreSpawnPlugin(modid = "basemetals", resourcePath = "orespawn")
+public class BaseMetalsOreSpawn implements IOreSpawnPlugin {
+
+	@Override
+	public void register(OS3API apiInterface) {
+		// nothing for us to do - all of our ores are in the
+		// jar and the code handles that
+	}
+}
