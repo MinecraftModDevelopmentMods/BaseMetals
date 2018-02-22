763eaac7a0344b85d2bef8ab7f290cae1b9e683c
Jasmine Iwanek <jasmine@iwanek.co.uk>
2018-02-22 02:29:42 -0800

    Fix waila integration

--- src/main/java/com/mcmoddev/lib/waila/Waila.java
+++ src/main/java/com/mcmoddev/lib/waila/Waila.java
@@ -12,7 +12,7 @@ public class Waila {
 	}
 
 	public static void init() {
-		FMLInterModComms.sendMessage("waila", "register", "com.mcmoddev.basemetals.waila.Waila.register");
+		FMLInterModComms.sendMessage("waila", "register", "com.mcmoddev.basemetals.lib.Waila.register");
 	}
 
 	public static void register(IWailaRegistrar registrar) {
