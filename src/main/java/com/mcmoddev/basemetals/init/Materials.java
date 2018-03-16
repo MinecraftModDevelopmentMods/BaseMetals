42272bd66886a0a2eb99f2d63bdefd1baf55c2cb
faceofcat <candreix@gmail.com>
2018-03-16 09:17:42 +0200

    moved vanilla materials to lib

--- src/main/java/com/mcmoddev/basemetals/init/Materials.java
+++ src/main/java/com/mcmoddev/basemetals/init/Materials.java
@@ -2,9 +2,7 @@ package com.mcmoddev.basemetals.init;
 
 import java.util.Arrays;
 import java.util.List;
-
 import javax.annotation.Nonnull;
-
 import com.mcmoddev.basemetals.data.MaterialNames;
 import com.mcmoddev.lib.data.SharedStrings;
 import com.mcmoddev.lib.material.MMDMaterial.MaterialType;
@@ -30,7 +28,6 @@ public final class Materials extends com.mcmoddev.lib.init.Materials {
 		// always created and populated with their base item-sets
 		// even if they are not enabled
 		// Oreless because our Recipe code can tend to be silly otherwise
-		createVanillaMats();
 
 		final List<String> materials = Arrays.asList(MaterialNames.ADAMANTINE, MaterialNames.ANTIMONY,
 				MaterialNames.BISMUTH, MaterialNames.COLDIRON, MaterialNames.COPPER, MaterialNames.LEAD,
@@ -227,22 +224,5 @@ public final class Materials extends com.mcmoddev.lib.init.Materials {
 			default:
 				return 1.0d;
 		}
-	}
-
-	private static void createVanillaMats() {
-		createOrelessMaterial(MaterialNames.WOOD, MaterialType.WOOD, 2, 2, 6, 0xFF695433);
-		createOrelessMaterial(MaterialNames.STONE, MaterialType.ROCK, 5, 4, 2, 0xFF8F8F8F);
-		createOrelessMaterial(MaterialNames.IRON, MaterialType.METAL, 8, 8, 4.5, 0xFFD8D8D8);
-		createOrelessMaterial(MaterialNames.GOLD, MaterialType.METAL, 1, 1, 10, 0xFFFFFF8B);
-		createOrelessMaterial(MaterialNames.DIAMOND, MaterialType.GEM, 10, 15, 4, 0xFF8CF4E1);
-		createOrelessMaterial(MaterialNames.COAL, MaterialType.MINERAL, 4, 4, 2, 0xFF151515);
-		createOrelessMaterial(MaterialNames.CHARCOAL, MaterialType.MINERAL, 4, 4, 2, 0xFF231F18);
-		createOrelessMaterial(MaterialNames.EMERALD, MaterialType.GEM, 10, 15, 4, 0xFF82F6AC);
-		createOrelessMaterial(MaterialNames.ENDER, MaterialType.GEM, 2, 2, 6, 0xFF063931);
-		createOrelessMaterial(MaterialNames.QUARTZ, MaterialType.GEM, 5, 4, 2, 0xFFEAE3DB);
-		createOrelessMaterial(MaterialNames.OBSIDIAN, MaterialType.ROCK, 10, 15, 4, 0xFF101019);
-		createOrelessMaterial(MaterialNames.LAPIS, MaterialType.MINERAL, 1, 1, 1, 0xFF26619c);
-		createOrelessMaterial(MaterialNames.PRISMARINE, MaterialType.MINERAL, 1, 1, 1, 0xFF7fb8a4);
-		createOrelessMaterial(MaterialNames.REDSTONE, MaterialType.MINERAL, 1, 1, 1, 0xFF720000);
 	}
 }
