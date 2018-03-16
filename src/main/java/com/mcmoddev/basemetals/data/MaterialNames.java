ecf69c7943d831689e15d43c50b95f7b25c7bd8e
Face of Cat <candreix@gmail.com>
2018-03-16 19:43:11 +0200

    Master 1.12 lib split (#308)
    * split 'lib' into 'mmdlib'
    
    * moved vanilla materials to lib

--- src/main/java/com/mcmoddev/basemetals/data/MaterialNames.java
+++ src/main/java/com/mcmoddev/basemetals/data/MaterialNames.java
@@ -1,47 +1,30 @@
 package com.mcmoddev.basemetals.data;
 
-import com.mcmoddev.lib.data.SharedStrings;
-
-public class MaterialNames {
-
+public class MaterialNames extends com.mcmoddev.lib.data.MaterialNames {
 	public static final String ADAMANTINE = "adamantine";
 	public static final String ANTIMONY = "antimony";
 	public static final String AQUARIUM = "aquarium";
 	public static final String BISMUTH = "bismuth";
 	public static final String BRASS = "brass";
 	public static final String BRONZE = "bronze";
-	public static final String COAL = "coal";
 	public static final String COLDIRON = "coldiron";
 	public static final String COPPER = "copper";
-	public static final String CHARCOAL = "charcoal";
 	public static final String CUPRONICKEL = "cupronickel";
-	public static final String DIAMOND = "diamond";
 	public static final String ELECTRUM = "electrum";
-	public static final String EMERALD = "emerald";
-	public static final String ENDER = "ender";
-	public static final String GOLD = "gold";
 	public static final String INVAR = "invar";
-	public static final String IRON = "iron";
-	public static final String LAPIS = "lapis";
 	public static final String LEAD = "lead";
 	public static final String MERCURY = "mercury";
 	public static final String MITHRIL = "mithril";
 	public static final String NICKEL = "nickel";
-	public static final String OBSIDIAN = "obsidian";
 	public static final String PEWTER = "pewter";
 	public static final String PLATINUM = "platinum";
-	public static final String PRISMARINE = "prismarine";
-	public static final String QUARTZ = "quartz";
-	public static final String REDSTONE = "redstone";
 	public static final String SILVER = "silver";
 	public static final String STARSTEEL = "starsteel";
 	public static final String STEEL = "steel";
-	public static final String STONE = "stone";
 	public static final String TIN = "tin";
-	public static final String WOOD = "wood";
 	public static final String ZINC = "zinc";
 
 	private MaterialNames() {
-		throw new IllegalAccessError(SharedStrings.NOT_INSTANTIABLE);
+		super();
 	}
 }
