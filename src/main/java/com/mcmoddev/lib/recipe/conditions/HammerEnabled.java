a67e5f41ace0481e678b3f719bf1d7547d2e6dbe
Daniel Hazelton <dshadowwolf@gmail.com>
2018-03-20 17:24:46 -0400

    blargh - needed this to fix an bug in the crackhammer handling - really just growing pains from the transition to JSON recipes.

--- src/main/java/com/mcmoddev/lib/recipe/conditions/HammerEnabled.java
+++ src/main/java/com/mcmoddev/lib/recipe/conditions/HammerEnabled.java
@@ -3,6 +3,7 @@ package com.mcmoddev.lib.recipe.conditions;
 import java.util.function.BooleanSupplier;
 
 import com.google.gson.JsonObject;
+import com.mcmoddev.lib.data.ConfigKeys;
 import com.mcmoddev.lib.util.ConfigBase.Options;
 
 import net.minecraftforge.common.crafting.IConditionFactory;
@@ -12,7 +13,7 @@ public class HammerEnabled implements IConditionFactory {
 
 	@Override
 	public BooleanSupplier parse(final JsonContext context, final JsonObject json) {
-		return () -> !Options.disableAllHammerRecipes();
+		return () -> !Options.disableAllHammerRecipes() && Options.isThingEnabled(ConfigKeys.CRACKHAMMER);
 	}
 
 }
