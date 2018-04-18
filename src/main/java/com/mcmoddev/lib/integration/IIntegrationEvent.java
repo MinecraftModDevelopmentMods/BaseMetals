dda306b17f7b7f77474f559dc436a00f1efa96a8
Daniel Hazelton <dshadowwolf@gmail.com>
2018-04-18 13:24:31 -0400

    okay, plugin backend work appears complete, for now - next is mod backend and then the plugins themselves

---
+++ src/main/java/com/mcmoddev/lib/integration/IIntegrationEvent.java
@@ -0,0 +1,12 @@
+package com.mcmoddev.lib.integration;
+import com.google.common.collect.Lists;
+import com.mcmoddev.lib.init.Materials;
+import com.mcmoddev.lib.material.MMDMaterial;
+
+import java.util.List;
+
+public interface IIntegrationEvent {
+	default List<MMDMaterial> materials() {
+		return Lists.newLinkedList(Materials.getAllMaterials());
+	}
+}
---
+++ src/main/java/com/mcmoddev/lib/integration/IntegrationPostInitEvent.java
@@ -0,0 +1,7 @@
+package com.mcmoddev.lib.integration;
+
+import net.minecraftforge.fml.common.eventhandler.Event;
+
+public class IntegrationPostInitEvent extends Event implements IIntegrationEvent {
+
+}
