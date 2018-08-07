6d41d0a2c168e6435b992496af25c4efce3fb85d
Daniel Hazelton <dshadowwolf@gmail.com>
2018-08-07 11:26:03 -0400

    add some javadoc where its better for me to do it than others

--- src/main/java/com/mcmoddev/lib/integration/IntegrationInitEvent.java
+++ src/main/java/com/mcmoddev/lib/integration/IntegrationInitEvent.java
@@ -2,6 +2,12 @@ package com.mcmoddev.lib.integration;
 
 import net.minecraftforge.fml.common.eventhandler.Event;
 
+/**
+ * Fired for any details that a plugin needs to run during the FMLInitializationEvent
+ * 
+ * @author D. Hazelton
+ *
+ */
 public class IntegrationInitEvent extends Event implements IIntegrationEvent {
 
 }
