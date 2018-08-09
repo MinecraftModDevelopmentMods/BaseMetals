61694259db1919e7f5d0650b7bb626a77818630e
Daniel Hazelton <dshadowwolf@gmail.com>
2018-08-09 00:16:14 -0400

    fix the 'use a plate to repair this' recipes... fecking changing API call semantics...

--- src/main/java/com/mcmoddev/lib/recipe/BootsRepairRecipe.java
+++ src/main/java/com/mcmoddev/lib/recipe/BootsRepairRecipe.java
@@ -2,6 +2,7 @@ package com.mcmoddev.lib.recipe;
 
 import com.mcmoddev.lib.data.Names;
 import com.mcmoddev.lib.material.MMDMaterial;
+import com.mcmoddev.lib.util.Oredicts;
 
 public class BootsRepairRecipe extends RepairRecipeBase {
 
