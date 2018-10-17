9e945d8bd5e916d79b3413849da52000b30b8b09
YaibaToKen <kurokyoudai@live.com>
2018-10-17 14:04:21 +0100

    Add isModLoaded check for users without ConArm

--- src/main/java/com/mcmoddev/lib/integration/plugins/ConstructsArmory.java
+++ src/main/java/com/mcmoddev/lib/integration/plugins/ConstructsArmory.java
@@ -1,24 +1,8 @@
 package com.mcmoddev.lib.integration.plugins;
 
-import com.google.common.collect.Queues;
-import com.mcmoddev.lib.integration.IntegrationInitEvent;
-import com.mcmoddev.lib.integration.IntegrationPostInitEvent;
-import com.mcmoddev.lib.integration.IntegrationPreInitEvent;
-import com.mcmoddev.lib.integration.plugins.tinkers.TinkerModifierRegistry;
-import com.mcmoddev.lib.integration.plugins.tinkers.TinkerTraitLocation;
-import com.mcmoddev.lib.integration.plugins.tinkers.TinkerTraitRegistry;
-import com.mcmoddev.lib.integration.plugins.tinkers.TinkersMaterial;
-import com.mcmoddev.lib.integration.plugins.tinkers.events.MaterialRegistrationEvent;
-import com.mcmoddev.lib.util.Config.Options;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.registries.IForgeRegistry;
-import slimeknights.tconstruct.library.TinkerRegistry;
+import c4.conarm.lib.materials.ArmorMaterials;
 import slimeknights.tconstruct.library.materials.Material;
 import slimeknights.tconstruct.library.traits.ITrait;
-
-import java.util.Deque;
-import java.util.Locale;
 
 /**
  * CA Plugin, redesigned.
@@ -26,7 +10,6 @@ import java.util.Locale;
  * @author Bruno Gaspar &lt;kurokyoudai@live.com&gt;
  * @since 2018-10-10
  */
-
 public class ConstructsArmory extends TinkersConstruct {
 
 	public static final String PLUGIN_MODID = "conarm";
@@ -36,12 +19,16 @@ public class ConstructsArmory extends TinkersConstruct {
 //	private static final IForgeRegistry<TinkersMaterial> materialsRegistry = TinkersConstruct.getMaterialsRegistry();
 //	private static final TinkerTraitRegistry traitsRegistry = TinkersConstruct.getTraitsRegistry(); // technically does nothing
 //	private static final TinkerModifierRegistry modifiersRegistry = TinkersConstruct.getModifiersRegistry(); // technically does nothing
-	
+
 	// other storage
 	public static final ConstructsArmory INSTANCE = new ConstructsArmory();
 
+	protected static void addArmorTrait(Material material, ITrait trait1, ITrait trait2){
+		ArmorMaterials.addArmorTrait(material, trait1, trait2);
+	}
+
 //	private static final Deque<Material> materialsToAdd = Queues.newArrayDeque();
-	
+
 //	@Override
 //	public void init() {
 //		if (!Options.isModEnabled(PLUGIN_MODID) || initDone) {
@@ -51,7 +38,7 @@ public class ConstructsArmory extends TinkersConstruct {
 //
 //		MinecraftForge.EVENT_BUS.register(this);
 //	}
-	
+
 //	/**
 //	 *
 //	 * @param event
