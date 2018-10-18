bab853022aa3bef5e057435e2977458e63e5fe00
YaibaToKen <kurokyoudai@live.com>
2018-10-18 14:50:58 +0100

    Implement ConArm trait toxic and poisonous. Update their descriptions

--- src/main/java/com/mcmoddev/lib/integration/plugins/armory/traits/TraitToxic.java
+++ src/main/java/com/mcmoddev/lib/integration/plugins/armory/traits/TraitToxic.java
@@ -1,28 +1,28 @@
 package com.mcmoddev.lib.integration.plugins.armory.traits;
 
 import c4.conarm.lib.traits.AbstractArmorTrait;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.MobEffects;
 import net.minecraft.item.ItemStack;
 import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.DamageSource;
 import net.minecraft.util.text.TextFormatting;
-import slimeknights.tconstruct.library.traits.AbstractTrait;
-
-import javax.annotation.Nonnull;
+import net.minecraftforge.common.util.FakePlayer;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
 
 /**
- * <h2><u>Toxic Tool Modifier:</u></h2>
+ * <h2><u>Toxic Armor Modifier:</u></h2>
  * <b>Name:</b> toxic
  * <br>
  * <b>Desc:</b>
- * Applies poison 2 for 5 seconds as well as blindness for 2.5 seconds when a living entity is hit.
+ * Applies poison 2 for 5 seconds as well as blindness for 2.5 seconds when a living entity hits the player.
  *
  * <br>
  * <b>String Reference:<br></b>
  * "toxic"
  * "mmd-toxic"
- *
- * @author Java doc author: Vase of Petunias
  */
 public class TraitToxic extends AbstractArmorTrait {
 
@@ -30,13 +30,13 @@ public class TraitToxic extends AbstractArmorTrait {
 		super("mmd-toxic", TextFormatting.GREEN);
 	}
 
-//	@Override
-//	public void afterHit(@Nonnull final ItemStack tool, @Nonnull final EntityLivingBase player,
-//			@Nonnull final EntityLivingBase target, @Nonnull final float damageDealt,
-//			@Nonnull final boolean wasCritical, @Nonnull final boolean wasHit) {
-//		if (wasHit && target.isEntityAlive()) {
-//			target.addPotionEffect(new PotionEffect(MobEffects.POISON, 100, 2));
-//			target.addPotionEffect(new PotionEffect(MobEffects.BLINDNESS, 50));
-//		}
-//	}
+	@Override
+	public float onHurt(ItemStack armor, EntityPlayer player, DamageSource source, float damage, float newDamage, LivingHurtEvent evt) {
+		Entity entity = source.getTrueSource();
+		if (entity instanceof EntityLivingBase && !(entity instanceof FakePlayer)) {
+			((EntityLivingBase) entity).addPotionEffect(new PotionEffect(MobEffects.POISON, 100, 2));
+			((EntityLivingBase) entity).addPotionEffect(new PotionEffect(MobEffects.BLINDNESS, 50));
+		}
+		return super.onHurt(armor, player, source, damage, newDamage, evt);
+	}
 }
