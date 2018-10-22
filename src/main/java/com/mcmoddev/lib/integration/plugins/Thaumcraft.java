e6c749ca7a3a91ad350aeeb354a6d94fdb70d3d3
YaibaToKen <kurokyoudai@live.com>
2018-10-22 14:17:11 +0100

    Added Praecantatio aspect to metals

--- src/main/java/com/mcmoddev/lib/integration/plugins/Thaumcraft.java
+++ src/main/java/com/mcmoddev/lib/integration/plugins/Thaumcraft.java
@@ -1,21 +1,15 @@
 package com.mcmoddev.lib.integration.plugins;
 
-import com.mcmoddev.basemetals.BaseMetals;
-import com.mcmoddev.basemetals.data.MaterialNames;
+import com.mcmoddev.basemetals.init.Materials;
+import com.mcmoddev.lib.data.Names;
 import com.mcmoddev.lib.integration.IIntegration;
+import com.mcmoddev.lib.material.MMDMaterial;
 import com.mcmoddev.lib.util.Config;
-import com.mcmoddev.lib.util.Config.Options;
-import com.mcmoddev.lib.util.Oredicts;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.common.registry.ForgeRegistries;
-import thaumcraft.api.ThaumcraftApi;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import thaumcraft.api.aspects.Aspect;
-import thaumcraft.api.aspects.AspectEventProxy;
 import thaumcraft.api.aspects.AspectList;
-
-import java.rmi.registry.Registry;
+import thaumcraft.api.aspects.AspectRegistryEvent;
 
 public class Thaumcraft implements IIntegration {
 
@@ -33,11 +27,67 @@ public class Thaumcraft implements IIntegration {
 		initDone = true;
 
 		MinecraftForge.EVENT_BUS.register(this);
-		registerAspectsForOreDictEntries();
 	}
 
-	private void registerAspectsForOreDictEntries() {
-		ItemStack itemStack = new ItemStack(ForgeRegistries.ITEMS.getValue(new ResourceLocation(BaseMetals.MODID + Oredicts.INGOT + MaterialNames.ADAMANTINE)));
-		ThaumcraftApi.registerComplexObjectTag(itemStack, (new AspectList()).add(Aspect.METAL, 33));
+	@SubscribeEvent
+	public void registerAspects(final AspectRegistryEvent ev) {
+		Materials.getAllMaterials().stream()
+				.filter( mat -> mat.hasOre())
+				.forEach( mat -> ev.register.registerComplexObjectTag(mat.getBlockItemStack(Names.ORE), (new AspectList()).add(Aspect.METAL, 33)) );
+		Materials.getAllMaterials().stream()
+				.filter( mat -> mat.hasItem(Names.INGOT))
+				.forEach( mat -> {
+					AspectList aspects = new AspectList();
+					addMagicAspect(aspects, mat);
+					addDesireAspect(aspects, mat);
+					aspects.add(Aspect.METAL, 33);
+					ev.register.registerComplexObjectTag(mat.getItemStack(Names.INGOT), aspects);
+				});
+	}
+
+	private AspectList addMetalAspect(AspectList aspectList, MMDMaterial material){
+		return aspectList.add(Aspect.METAL, getMetalAspect(material));
+	}
+
+	private int getMetalAspect(MMDMaterial material){
+		return 33;
+	}
+
+	private AspectList addDesireAspect(AspectList aspectList, MMDMaterial material){
+		int value = getDesireAspect(material);
+		if(value > 0){
+			aspectList.add(Aspect.DESIRE, value);
+		}
+		return aspectList;
+	}
+
+	private int getDesireAspect(MMDMaterial material){
+		return getDesireAspect(material.isRare());
+	}
+
+	private int getDesireAspect(boolean isRare){
+		if(isRare){
+			return 8;
+		}
+		return 0;
+	}
+
+	private AspectList addMagicAspect(AspectList aspectList, MMDMaterial material){
+		int value = getMagicAspect(material);
+		if(value > 0){
+			aspectList.add(Aspect.MAGIC, value);
+		}
+		return aspectList;
+	}
+
+	private int getMagicAspect(MMDMaterial material){
+		return getMagicAspect(material.getEnchantability());
+	}
+
+	private int getMagicAspect(float enchantability){
+		if(enchantability >= 15){
+			return (int)(enchantability * 3);
+		}
+		return 0;
 	}
 }
