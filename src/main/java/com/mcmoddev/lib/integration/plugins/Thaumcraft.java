781e61cdbbe9d21d239c2906a2b1942da8afc222
YaibaToKen <kurokyoudai@live.com>
2018-10-25 10:13:24 +0100

    Revert "Revert "Basic aspects are automatically assigned to all of the materials. Somes blocks / items still require proper multipliers for the aspects amounts.""
    This reverts commit e9654e70e70be2832a57f8407094995ce44dc74e.

--- src/main/java/com/mcmoddev/lib/integration/plugins/Thaumcraft.java
+++ src/main/java/com/mcmoddev/lib/integration/plugins/Thaumcraft.java
@@ -1,6 +1,5 @@
 package com.mcmoddev.lib.integration.plugins;
 
-import com.mcmoddev.basemetals.BaseMetals;
 import com.mcmoddev.lib.data.Names;
 import com.mcmoddev.lib.integration.IIntegration;
 import com.mcmoddev.lib.integration.IntegrationPreInitEvent;
@@ -9,7 +8,6 @@ import com.mcmoddev.lib.integration.plugins.thaumcraft.TCMaterial;
 import com.mcmoddev.lib.integration.plugins.thaumcraft.TCSyncEvent;
 import com.mcmoddev.lib.material.MMDMaterial;
 import com.mcmoddev.lib.util.Config;
-import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -49,7 +47,12 @@ public class Thaumcraft implements IIntegration {
 	}
 
 	public static float getPartMultiplier(String name){
-		return partMultiplierMap.get(name);
+		if(partMultiplierMap.containsKey(name)){
+			return partMultiplierMap.get(name);
+		}
+		else{
+			return 0f;
+		}
 	}
 	
 	@Override
@@ -64,9 +67,16 @@ public class Thaumcraft implements IIntegration {
 
 		putPartMultiplier(Names.NUGGET, 1f);
 		putPartMultiplier(Names.INGOT, 9f);
-		putPartMultiplier(Names.GEM, getPartMultiplier(Names.INGOT));
-		putPartMultiplier(Names.BLEND, getPartMultiplier(Names.INGOT) * 0.8f);
+		putPartMultiplier(Names.BLOCK, getPartMultiplier(Names.INGOT) * 9);
 		putPartMultiplier(Names.ORE, getPartMultiplier(Names.INGOT));
+
+		putPartMultiplier(Names.GEM, getPartMultiplier(Names.INGOT));
+
+		putPartMultiplier(Names.BLEND, getPartMultiplier(Names.INGOT) * 0.8f);
+		putPartMultiplier(Names.SMALLBLEND, getPartMultiplier(Names.BLEND) / 9);
+
+		putPartMultiplier(Names.POWDER, getPartMultiplier(Names.BLEND));
+		putPartMultiplier(Names.SMALLPOWDER, getPartMultiplier(Names.SMALLBLEND));
 	}
 	
 	@SubscribeEvent
@@ -76,28 +86,37 @@ public class Thaumcraft implements IIntegration {
 
 	@SubscribeEvent
 	public void registerAspects(final AspectRegistryEvent ev) {
+		// TODO Rewrite this piece of crap with efficiency in mind
 		registry.getValuesCollection().stream()
 				.forEach( tcMaterial -> tcMaterial.getAspectMapKeys()
-						.forEach( key -> ev.register.registerComplexObjectTag(new ItemStack(tcMaterial.getItem(key)),tcMaterial.getAspectFor(key))));
+						.forEach( key -> ev.register.registerComplexObjectTag(tcMaterial.getItemStack(key),tcMaterial.getAspectFor(key))));
+
+		registry.getValuesCollection().stream()
+				.forEach( tcMaterial -> tcMaterial.getAspectMapKeys()
+						.forEach( key -> ev.register.registerComplexObjectTag(tcMaterial.getBlockItemStack(key),tcMaterial.getAspectFor(key))));
 	}
 
-	public static TCMaterial createPartsAspects(MMDMaterial material, String... names){
-		TCMaterial tcMaterial = new TCMaterial(material);
-		for (String name:names) {
-			tcMaterial.addAspect(name, AspectsMath.addAspects(material, name));
-		}
-		return tcMaterial;
+	public static TCMaterial createVanillaWithAspects(MMDMaterial material){
+		return Thaumcraft.createPartsAspects(material, Names.NUGGET, Names.BLEND, Names.SMALLBLEND, Names.POWDER, Names.SMALLPOWDER);
+	}
+
+	public static TCMaterial createWithAspects(MMDMaterial material){
+		return createPartsAspects(material, Names.values());
 	}
 
 	public static TCMaterial createPartsAspects(MMDMaterial material, Names... names){
 		TCMaterial tcMaterial = new TCMaterial(material);
 		for (Names name:names) {
-			tcMaterial.addAspect(name, AspectsMath.addAspects(material, name.toString()));
+			tcMaterial.addAspect(name, AspectsMath.getAspects(material, name));
 		}
 		return tcMaterial;
 	}
 
-	public static TCMaterial createWithAspects(MMDMaterial material){
-		return createPartsAspects(material, Names.NUGGET, Names.INGOT, Names.ORE, Names.GEM, Names.BLEND);
+	public static TCMaterial createPartsAspects(MMDMaterial material, String... names){
+		TCMaterial tcMaterial = new TCMaterial(material);
+		for (String name:names) {
+			tcMaterial.addAspect(name, AspectsMath.getAspects(material, name));
+		}
+		return tcMaterial;
 	}
 }
