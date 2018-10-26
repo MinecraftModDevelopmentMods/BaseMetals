a0b96edfd7af6a24d61f1772eb3da252d8c961dd
Daniel "DShadowWolf" Hazelton <dshadowwolf@gmail.com>
2018-10-26 12:17:42 -0400

    work on making AspectMath have a set of defaults that can be overridden

--- src/main/java/com/mcmoddev/lib/integration/plugins/thaumcraft/TCMaterial.java
+++ src/main/java/com/mcmoddev/lib/integration/plugins/thaumcraft/TCMaterial.java
@@ -5,6 +5,7 @@ import com.mcmoddev.lib.material.MMDMaterial;
 import com.mcmoddev.lib.data.NameToken;
 import com.mcmoddev.lib.data.Names;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
@@ -20,10 +21,12 @@ import thaumcraft.api.aspects.AspectList;
 public class TCMaterial extends IForgeRegistryEntry.Impl<TCMaterial> implements IMMDObject {
     private MMDMaterial baseMaterial;
     private Map<String, AspectList> aspectMap;
+    private final Map<NameToken, Map<Aspect, IAspectCalculation>> aspectCalcs;
     
     public TCMaterial(MMDMaterial baseMaterial) {
         this.baseMaterial = baseMaterial;
         this.aspectMap = new TreeMap<>();
+        this.aspectCalcs = new HashMap<>();
         super.setRegistryName(this.baseMaterial.getRegistryName());
     }
 
@@ -31,6 +34,44 @@ public class TCMaterial extends IForgeRegistryEntry.Impl<TCMaterial> implements 
         return aspectMap.keySet();
     }
 
+    public void addAspectCalculation(Names part, Aspect aspect, IAspectCalculation calculationFunc) {
+    	this.addAspectCalculation(part.toString(), aspect, calculationFunc);
+    }
+    
+    public void addAspectCalculation(String part, Aspect aspect, IAspectCalculation calculationFunc) {
+    	this.addAspectCalculation(new NameToken(part), aspect, calculationFunc);
+    }
+    
+    public void addAspectCalculation(NameToken part, Aspect aspect, IAspectCalculation calculationFunc) {
+    	Map<Aspect, IAspectCalculation> vals = this.aspectCalcs.getOrDefault(part, new TreeMap<Aspect, IAspectCalculation>());
+    	vals.put(aspect, calculationFunc);
+    	this.aspectCalcs.put(part, vals);
+    }
+    
+    public boolean hasCalcFor(Names part) {
+    	return this.hasCalcFor(part.toString());
+    }
+    
+    public boolean hasCalcFor(String part) {
+    	return this.hasCalcFor(new NameToken(part));
+    }
+    
+    public boolean hasCalcFor(NameToken part) {
+    	return this.aspectCalcs.containsKey(part);
+    }
+    
+    public Map<Aspect, IAspectCalculation> getCalcsFor(Names part) {
+    	return this.getCalcsFor(part.toString());
+    }
+    
+    public Map<Aspect, IAspectCalculation> getCalcsFor(String part) {
+    	return this.getCalcsFor(new NameToken(part));    	
+    }
+    
+    public Map<Aspect, IAspectCalculation> getCalcsFor(NameToken part) {
+    	return this.aspectCalcs.getOrDefault(part, null);
+    }
+    
     public TCMaterial addAspect(Names part, Aspect aspect, int amount){
     	if (this.aspectMap.containsKey(part.toString())) {
     		AspectList al = this.aspectMap.get(part.toString());
