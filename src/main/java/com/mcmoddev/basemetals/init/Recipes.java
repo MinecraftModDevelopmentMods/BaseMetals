c46738c5b8b28c94736d5ba4f7aeeafb404f43f6
dshadowwolf <dshadowwolf@gmail.com>
2019-05-17 00:32:45 -0400

    Final round of fixes to prep for -rc2 (#441)
    * the JEI stuff is in MMDLib, not BaseMetals - moved there
    
    * move almost all of the bits from the 'general' config category out to MMDLib
    
    * Lots of bug fixes
    Undo brain-dead idea of moving vanilla extension to MMDLib
    
    * update the changelog
    
    * update version to -rc2
    
    * fix all the niggling issues that were outstanding in the return of extended vanilla to BMe and other edge-case cruft that was showing in the logs - also remove debugging outs that were only needed for a special-case
    
    * fix issues in lang files, find and fix bug in MMDLib that had an impact here, add a call to dump the furnace recipes... which will be removed in the next commit
    
    * remove furnace recipe dump, restore oredictionary extras for Adamantine

--- src/main/java/com/mcmoddev/basemetals/init/Recipes.java
+++ src/main/java/com/mcmoddev/basemetals/init/Recipes.java
@@ -1,6 +1,5 @@
 package com.mcmoddev.basemetals.init;
 
-import com.mcmoddev.basemetals.BaseMetals;
 import com.mcmoddev.basemetals.data.MaterialNames;
 import com.mcmoddev.lib.data.Names;
 import com.mcmoddev.lib.data.SharedStrings;
@@ -11,6 +10,7 @@ import com.mcmoddev.lib.util.Oredicts;
 import net.minecraft.item.crafting.IRecipe;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.oredict.OreDictionary;
 
@@ -32,7 +32,7 @@ public final class Recipes extends com.mcmoddev.lib.init.Recipes {
 		MinecraftForge.EVENT_BUS.register(Recipes.class);
 	}
 
-	private static void initModSpecificRecipes() {
+	private static void initModSpecificOredicts() {
 		if (Materials.hasMaterial(MaterialNames.ADAMANTINE)) {
 			final MMDMaterial adamantine = Materials.getMaterialByName(MaterialNames.ADAMANTINE);
 
@@ -61,9 +61,9 @@ public final class Recipes extends com.mcmoddev.lib.init.Recipes {
 	 * @param event
 	 *            The Event.
 	 */
-	@SubscribeEvent
+	@SubscribeEvent(priority=EventPriority.LOWEST)
 	public static void registerRecipes(final RegistryEvent.Register<IRecipe> event) {
-		initModSpecificRecipes();
+		initModSpecificOredicts();
 		register(event);		
 	}
 }
