784965bc1389d137c2ff38b57c1122842a4c1e61
dshadowwolf <dshadowwolf@gmail.com>
2019-10-14 18:51:09 -0400

    move minecraft world getter to proxy method (#459)
    * 1) Move adding existing items for vanilla stuff to MMDLib
    2) Move adding existing blocks for vanilla stuff to MMDLib
    3) Move configuring "vanilla materials" on/off to MMDLib
    -- This allows for MMDLib to provide for access to existing vanilla stuff
    without providing anything extra
    
    * fix the crash caused by aquarium properties server-side
    
    * no real change - forcing a re-run of Travis now that the integration is fixed

--- src/main/java/com/mcmoddev/basemetals/proxy/ServerProxy.java
+++ src/main/java/com/mcmoddev/basemetals/proxy/ServerProxy.java
@@ -1,5 +1,10 @@
 package com.mcmoddev.basemetals.proxy;
 
+import javax.annotation.Nullable;
+
+import net.minecraft.world.World;
+import net.minecraftforge.fml.common.FMLCommonHandler;
+
 /**
  * Base Metals Server Proxy.
  *
@@ -8,4 +13,8 @@ package com.mcmoddev.basemetals.proxy;
  */
 public final class ServerProxy extends CommonProxy {
 	// Nothing to see here people
+	@Override
+	public World getWorld(@Nullable final int dimension) {
+		return FMLCommonHandler.instance().getMinecraftServerInstance().getWorld(dimension);
+	}
 }
